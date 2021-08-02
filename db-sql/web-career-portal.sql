0abc2f7cefbc1adccba1b1d8ca56a0577351b98f
Nick <lamothe.dev@gmail.com>
2021-08-02 15:44:18 -0400

    sql modifications

--- db-sql/web-career-portal.sql
+++ db-sql/web-career-portal.sql
@@ -1,7 +1,15 @@
 -- COMP 5531 - Web Career Portal
 
--- Very simple beginnings of the database. Starting slow.
-
+-- NOTE: We create this because we potentially must associate a username with a given employer
+--       and we also need to associate an employer with each job posting
+CREATE TABLE `employer` (
+    `employer_ID` VARCHAR(255),
+    `name` VARCHAR(255),
+    `address` VARCHAR(255)
+    `phone` VARCHAR(255),
+    `email` VARCHAR(255),
+    PRIMARY KEY (employer_ID)
+);
 
 -- NOTE: `total_jobs_posted` will be used by an employer, and
 --       `total_applications_submitted` will be used by a user (who is looking for jobs);
@@ -9,17 +17,19 @@
 
 --        this might look different after normalization
 
+-- NOTE:  default from `employer_ID` is NULL in case the user is not an employer
 CREATE TABLE 'user' (
     `username` VARCHAR(255),
-    `user_type` VARCHAR(255), -- admin, employer, user
+    `employer_ID` VARCHAR(255) NULL,
     `first_name` VARCHAR(255),
     `last_name` VARCHAR(255),
-    `email_address` VARCHAR(255),
+    `email` VARCHAR(255),
     `password` VARCHAR(255),
     `security_answer` VARCHAR(255),
     `total_jobs_posted` INT,
     `total_applications_submitted` INT,
-    PRIMARY KEY (username)
+    PRIMARY KEY (username),
+    FOREIGN KEY (employer_ID) REFERENCES employer (employer_ID)
 );
 
 -- Only two payment methods, so they'll always be unique here
@@ -28,24 +38,24 @@ CREATE TABLE `payment_method` (
     PRIMARY KEY (payment_method)
 );
 
--- Each payment is unique, and the ID will determine everything, including username
-CREATE TABLE `payment` (
-    `payment_ID` VARCHAR(255),
-    `username` VARCHAR(255),
-    `payment_total` INT(3),
-    `payment_method` VARCHAR(255),
-    `withdrawal_type` VARCHAR(255), -- manual, auto
-    PRIMARY KEY (payment_ID),
-    FOREIGN KEY (username) REFERENCES payment_method (payment_method),
-    FOREIGN KEY (payment_method) REFERENCES payment_method (payment_method)
-);
-
 -- Creating the account separately so that we can associate multiple payment methods with a user
 CREATE TABLE `user_account` (
     `username` VARCHAR(255),
     `payment_method` VARCHAR(255),
     PRIMARY KEY (username, payment_method),
     FOREIGN KEY (username) REFERENCES user (username)
+);
+
+-- Each payment is unique, and the ID will determine everything, including username
+CREATE TABLE `payment` (
+    `payment_ID` VARCHAR(255),
+    `username` VARCHAR(255) NOT NULL,
+    `payment_total` INT(3),
+    `payment_method` VARCHAR(255),
+    `withdrawal_type` VARCHAR(255), -- manual, auto
+    PRIMARY KEY (payment_ID),
+    FOREIGN KEY (username) REFERENCES payment_method (payment_method),
+    FOREIGN KEY (payment_method) REFERENCES payment_method (payment_method)
 );
 
 -- Only two job categories, so they'll always be unique here
@@ -58,26 +68,27 @@ CREATE TABLE `job_category` (
 -- Each job is unique, and so the ID will determine everything
 CREATE TABLE `job` (
     `job_ID` VARCHAR(255),
-    `job_title` VARCHAR(255),
+    `employer_ID` VARCHAR(255),
     `job_category` VARCHAR(255),
-    `job_salary` INT,
-    `job_description` VARCHAR(MAX), -- potentially much larger text
+    `title` VARCHAR(255),
+    `salary` INT,
+    `description` VARCHAR(MAX), -- potentially much larger text
     `date_posted` DATE,
     `date_start` DATE,
     PRIMARY KEY (job_ID),
+    FOREIGN KEY (employer_ID) REFERENCES employer (employer_ID),
     FOREIGN KEY (job_category) REFERENCES job_category (job_category)
 );
 
--- TODO: add more attributes, potentially a CV, etc.
 CREATE TABLE `job_application` (
     `username` VARCHAR(255),
     `job_ID` VARCHAR(255),
+    `application` VARCHAR(255),
     `application_status` VARCHAR(255), -- accepted, rejected
     PRIMARY KEY (username, job_ID),
     FOREIGN KEY (username) REFERENCES user (username),
     FOREIGN KEY (job_ID) REFERENCES job (job_ID)
 );
-
 
 -- Additional notes to inform potential relations to be made:
 
